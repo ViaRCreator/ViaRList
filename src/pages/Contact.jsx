554dc94920460f89a51a064aa7ba45391995a5cb
ViaR <github.outsider210@slmails.com>
2026-08-03 21:07:43 +0000

    Create Contact.jsx

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Contact(){
+  return (
+    <section className="container">
+      <h1>Contact</h1>
+      <p>If you want to be added or have questions, open an issue or pull request on the repository.</p>
+    </section>
+  )
+}
