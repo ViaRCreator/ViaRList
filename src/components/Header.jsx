692369ca582f25199c2bb8c6d2b0d8dbbba140b3
ViaR <github.outsider210@slmails.com>
2026-08-04 21:35:52 +0000

    Localize header links using useI18n hook

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,21 +2,25 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import LocaleToggle from './LocaleToggle'
 import ThemeToggle from './ThemeToggle'
+import { useI18n } from '../i18n'
 
 export default function Header(){
+  const { t } = useI18n()
   return (
     <header className="header">
       <div className="header-inner container">
         <div className="brand">
-          <Link to="/">ViaRList</Link>
+          <Link to="/">{t('title')}</Link>
         </div>
         <nav>
-          <Link to="/about">About</Link>
-          <Link to="/contact">Contact</Link>
+          <Link to="/">{t('feed')}</Link>
+          <Link to="/about">{t('about')}</Link>
+          <Link to="/contact">{t('contact')}</Link>
           <LocaleToggle />
           <ThemeToggle />
         </nav>
       </div>
+      <div className="banner" aria-hidden="true"></div>
     </header>
   )
 }
