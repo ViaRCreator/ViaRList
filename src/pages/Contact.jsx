9a714aa78c9fcd39b7fdd175084f05dc9fe95fcb
ViaR <github.outsider210@slmails.com>
2026-08-04 22:09:05 +0000

    Localize Contact page content with i18n

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { useI18n } from '../i18n'
 
 export default function Contact(){
+  const { t } = useI18n()
   return (
     <section className="container">
-      <h1>Contact</h1>
-      <p>If you want to be added or have questions, open an issue or pull request on the repository.</p>
+      <h1>{t('contact')}</h1>
+      <p>{t('contact_text') || t('how_to_contribute_steps')}</p>
+      <p>Or if you prefer, contact the maintainer(ViaRCreator on X/Twitter).</p>
     </section>
   )
 }
